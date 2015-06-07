9d3343fc7995ae6d9533a35a6d0b155068843ecf
MauricioNO <mau_mau11@hotmail.com>
2015-06-07 11:54:13 -0400

    Create constants.h

---
+++ HW5/constants.h
@@ -0,0 +1,50 @@
+#ifndef CONSTANTS_H
+#define CONSTANTS_H
+
+
+extern int counter;	//Global declaration of the time keep of the world
+
+namespace Constants
+{
+	// forward declarations only
+	extern const int x_window;
+	extern const int y_window;
+	extern const int x_boundry;
+	extern const int y_boundry;
+	extern const int grass_width;
+	extern const int grass_height;
+	extern const int refresh_rate;
+	extern const int cow_width;
+	extern const int cow_height;
+	extern const int snake_width;
+	extern const int snake_height;
+	extern const int eagle_width;
+	extern const int eagle_height;
+	extern const int rabbit_width;
+	extern const int rabbit_height;
+	extern const int plankton_height;
+	extern const int plankton_width;
+	extern const int fish_height;
+	extern const int fish_width;
+	extern const int crocodile_height;
+	extern const int crocodile_width;
+	extern const int whale_height;
+	extern const int whale_width;
+	extern const int human_height;
+	extern const int human_width;
+
+	extern const int maxMoveSpeed;
+
+	extern const int cow_maxHunger;
+	extern const int snake_maxHunger;
+	extern const int eagle_maxHunger;
+	extern const int rabbit_maxHunger;
+	extern const int fish_maxHunger;
+	extern const int crocodile_maxHunger;
+	extern const int whale_maxHunger;
+	extern const int kraken_maxHunger;
+	extern const int human_maxHunger;
+}
+
+
+#endif
