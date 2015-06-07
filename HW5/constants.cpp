5d3b62f45d845314c68e3a10fc83654407220b52
MauricioNO <mau_mau11@hotmail.com>
2015-06-07 11:54:00 -0400

    Create constants.cpp

---
+++ HW5/constants.cpp
@@ -0,0 +1,44 @@
+#include "constants.h"
+namespace Constants
+{
+	extern const int x_window(800);
+	extern const int y_window(800);
+	extern const int x_boundry(799);
+	extern const int y_boundry(799);
+	extern const int grass_width(8);
+	extern const int grass_height(8);
+	extern const int refresh_rate(100);	//In milliseconds
+	extern const int cow_width(16);
+	extern const int cow_height(16);
+	extern const int snake_width(4);
+	extern const int snake_height(20);
+	extern const int eagle_width(30);
+	extern const int eagle_height(8);
+	extern const int rabbit_width(10);
+	extern const int rabbit_height(10);
+	extern const int plankton_height(2);
+	extern const int plankton_width(2);
+	extern const int fish_height(6);
+	extern const int fish_width(11);
+	extern const int crocodile_height(9);
+	extern const int crocodile_width(18);
+	extern const int whale_height(30);
+	extern const int whale_width(20);
+	extern const int human_height(28);
+	extern const int human_width(9);
+
+
+
+
+	extern const int maxMoveSpeed(10); //No one has 10 yet
+
+	extern const int cow_maxHunger(3);
+	extern const int snake_maxHunger(1);
+	extern const int eagle_maxHunger(1);
+	extern const int rabbit_maxHunger(1);
+	extern const int fish_maxHunger(5);
+	extern const int crocodile_maxHunger(2);
+	extern const int whale_maxHunger(4);
+	extern const int human_maxHunger(1);
+
+}
